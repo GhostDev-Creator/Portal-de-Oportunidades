ae7f284747ca331434d3dfa25a9feacf380a352a
! Ghost <115671461+GhostDev-Creator@users.noreply.github.com>
2025-10-06 07:17:39 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('contatoForm');
+const feedbackElement = document.getElementById('feedback');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    
+    const nome = formData.get('nome')
+    const email = formData.get('email')
+    const assunto = formData.get('assunto')
+    const mensagem = formData.get('mensagem')
+
+    console.log("--- Envio do Email ---");
+    console.log("Dados enviados: ");
+    console.log("nome: ", nome);
+    console.log("email: ", email);
+    console.log("assunto :",assunto);
+    console.log("mensagem: ", mensagem);
+    console.log("----------------------");
+
+    feedbackElement.textContent = "Dados capturados! Verifique a sua caixa de entrada (Console do Site (F12)).";
+    feedbackElement.style.color = "green";
+
+    form.reset();
+
+    setTimeout(() => {
+        feedbackElement.textContent = '';
+    }, 1500);
+})
